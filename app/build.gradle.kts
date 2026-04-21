acae8cb86b53a4d01d7415aecf38682a75bb9e4f
boudour ayari <ayariboudour@gmail.com>
2026-04-21 16:16:20 +0200

    addbuttons functionality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+    implementation("com.google.android.material:material:1.13.0")
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
--- app/src/main/java/com/example/selfpromoapp/MainActivity.kt
+++ app/src/main/java/com/example/selfpromoapp/MainActivity.kt
@@ -1,14 +1,31 @@
 package com.example.selfpromoapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val previewButton: Button = findViewById(R.id.button_preview_mesage)
+        previewButton.setOnClickListener {
+            onPreviewClicked()
+        }
     }
+
+    private fun onPreviewClicked() {
+        val contactNameEditText: TextInputEditText = findViewById(R.id.edit_text_contact_name)
+        val contactNumberEditText: TextInputEditText = findViewById(R.id.edit_text_contact_number)
+        val testString = contactNameEditText.text.toString()+ " "+ contactNumberEditText.text.toString()
+        Toast.makeText(this, testString, Toast.LENGTH_LONG).show()
+
+    }
+
 }

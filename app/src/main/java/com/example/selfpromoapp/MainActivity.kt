fed0042947f7ae1066952ac37f66dc22d3538ea5
boudour ayari <ayariboudour@gmail.com>
2026-04-22 13:42:57 +0200

    add intialization

--- app/src/main/java/com/example/selfpromoapp/MainActivity.kt
+++ app/src/main/java/com/example/selfpromoapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.selfpromoapp
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,9 +13,27 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private var contactNameEditText: TextInputEditText? = null
+    private var contactNumberEditText: TextInputEditText? = null
+    private var myDisplayNameEditText: TextInputEditText? = null
+    private var includeJuniorCheckBox: CheckBox? = null
+    private var startDateEditText: TextInputEditText? = null
+    private var immediateStartCheckBox: CheckBox? = null
+    private var jobTitleSpinner: Spinner? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        contactNameEditText = findViewById(R.id.edit_text_contact_name)
+        contactNumberEditText = findViewById(R.id.edit_text_contact_number)
+        myDisplayNameEditText = findViewById(R.id.edit_text_contact_my_display_name)
+        includeJuniorCheckBox = findViewById(R.id.check_box_junior)
+        startDateEditText = findViewById(R.id.edit_text_start_date)
+        immediateStartCheckBox = findViewById(R.id.check_box_immediate_start)
+        jobTitleSpinner = findViewById(R.id.spinner_job_title)
+
 
         val previewButton: Button = findViewById(R.id.button_preview_mesage)
         previewButton.setOnClickListener {
@@ -21,9 +42,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onPreviewClicked() {
-        val contactNameEditText: TextInputEditText = findViewById(R.id.edit_text_contact_name)
-        val contactNumberEditText: TextInputEditText = findViewById(R.id.edit_text_contact_number)
-        val testString = contactNameEditText.text.toString()+ " "+ contactNumberEditText.text.toString()
+        val testString = contactNameEditText!!.text.toString()+ ", "+ contactNumberEditText!!.text.toString()
+        Log.e("MainActivity", "Preview button clicked: $testString")
         Toast.makeText(this, testString, Toast.LENGTH_LONG).show()
 
     }
